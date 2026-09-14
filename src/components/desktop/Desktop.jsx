4f50f4af077df5c70a0d4da24338f1f7320530a7
Fazb3r <faiberpiedrahita@gmail.com>
2026-09-14 17:36:10 -0500

    feat: replace projects SVG with custom pink folder.webp icon

--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -49,12 +49,16 @@ function useClock() {
   return time;
 }
 
-// Sleek Feather Icons
+// Custom Desktop Icons
 function FolderIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-    </svg>
+    <img 
+      src="/assets/folder.webp" 
+      alt="Projects Folder" 
+      width="56" 
+      height="56" 
+      style={{ objectFit: 'contain' }} 
+    />
   );
 }
 
